9eddfe97b772ed33cc967012e78eb8b098ec3cc7
Marcos Borges <110690829+marcosborg@users.noreply.github.com>
2026-02-25 07:35:15 +0000

    update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,8 +41,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\SetLocale::class,
         ],
         'api' => [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\AuthGates::class,
